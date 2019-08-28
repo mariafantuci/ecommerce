43359fe557d04f6f71cce95ad824f750ba1a1004
Maria_Fantuci <mariafantuci@gmail.com>
2019-08-27 21:56:32 -0300

    Tela de login

---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Hcode;
+
+class Model{
+	private $values = [];
+
+	public function __call($name, $args){
+		$method = substr($name, 0, 3);
+		$fieldName = substr($name, strlen($name));
+		switch ($method) {
+			case "get":
+				return  $this->values[$fieldName]
+				break;
+			
+			case "set":
+				$this->values[$fieldName] = $args[0];
+				break;
+		}
+	}
+
+	public function setData($data = array()){
+		foreach ($data as $key => $value) {
+			$this->{"set".$key}($value);
+		}
+	}
+
+	public function getValues(){
+		$this->values;
+	}
+}
+
+
+?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,10 +9,13 @@
  	private $tpl;
  	private $options = [];//vetor vazio
  	private $defaults = [
+ 		"header"=>true,
+ 		"footer"=>true,
  		"data"=>[]
-
  	];
+
  	public function __construct($opts = array(), $tpl_dir = "/views/"){
+ 		$this->defaults["data"]["session"] = $_SESSION;
 
  		$this->options = array_merge($this->defaults, $opts);
 
@@ -21,26 +24,27 @@
 			"cache_dir" =>	$_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug" 	=> 	false // set to false to improve the speed
 		);
+		
 		Tpl::configure( $config );
 		$this->tpl = new Tpl;
 		$this->setData($this->options["data"]);
-		$this->tpl->draw("header");
+		if($this->options["header"] === true) $this->tpl->draw("header");
  	}
 
  	private function setData($data = array()){
  		foreach ($data as $key => $value) {
 			$this->tpl->assign($key, $value);
-		}	
+		}
+
  	}
 
- 	public function setTpl($name, $data = array(), $returnHTML = false){
+ 	public function setTpl($name, $data = array(), $returnHTML = false){//templete do html
  		$this->setData($data);
  		return $this->tpl->draw($name, $returnHTML);
-
  	}
 
  	public function __destruct(){
- 		$this->tpl->draw("footer");
+ 		if($this->options["footer"] === true) $this->tpl->draw("footer");
  	}
  }
 
